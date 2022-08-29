4d6d17da0d06bea0f1646c05208ce75d84a89d49
andregom <andre.oliveira@ccc.ufcg.edu.br>
2022-08-29 01:04:34 -0300

    refactoring way of showing right answer

--- src/components/question/index.tsx
+++ src/components/question/index.tsx
@@ -1,5 +1,6 @@
-import { useState } from "react";
+import React, { useRef, useState } from "react";
 
+import './styles.css';
 export interface MultiChoiceQuestionProps {
     question: {
         questions: string[],
@@ -12,6 +13,8 @@ const MultiChoiceQuestion: React.FC<MultiChoiceQuestionProps> = (props) => {
 
     const NUMBER_OF_QUESTIONS = props.question.questions.length
 
+    const options = useRef<HTMLUListElement>(null);
+
     const [questionIndex, setquestionIndex] = useState<number>(0);
 
     const SwitchIndex = () => {
@@ -22,14 +25,29 @@ const MultiChoiceQuestion: React.FC<MultiChoiceQuestionProps> = (props) => {
             setquestionIndex(0)
     }
 
+    const checkAnswer = (optionsList: HTMLUListElement) => {
+        const alternatives = Object.values(optionsList.children);
+        alternatives.forEach(alternative => {
+            if (alternative.textContent === props.question.answer)
+                alternative.className = 'right-answer';
+        });
+    }
+
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        if (options.current) {
+            checkAnswer(options.current)
+        }
+    }
+
     return (
         <div className="question-container">
             <span className="question-body">
                 {props.question.questions[questionIndex]}
                 <button onClick={SwitchIndex}>Rephrase</button>
             </span>
-            <form>
-                <ul>
+            <form onSubmit={e => {handleSubmit(e)}}>
+                <ul ref={options}>
                     {props.question.alternatives.map(item => (
                         <li className="alternative">
                             <input name={props.question.questions[0]} type="radio" value={item} />
@@ -37,6 +55,7 @@ const MultiChoiceQuestion: React.FC<MultiChoiceQuestionProps> = (props) => {
                         </li>
                     ))}
                 </ul>
+                <button className="check-button" type="submit">check</button>
             </form>
         </div>
     );

23c8588a388592354d070a0ad78cc74c35870900
andregom <andre.oliveira@ccc.ufcg.edu.br>
2022-08-29 18:31:49 -0300

    clears right answer decoration on content change

--- src/components/question/index.tsx
+++ src/components/question/index.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState, useMemo } from "react";
 
 import './styles.css';
 export interface MultiChoiceQuestionProps {
@@ -15,7 +15,12 @@ const MultiChoiceQuestion: React.FC<MultiChoiceQuestionProps> = (props) => {
 
     const options = useRef<HTMLUListElement>(null);
 
+    const [data, setData] = useState(props);
     const [questionIndex, setquestionIndex] = useState<number>(0);
+
+    useEffect(() => {
+        clearAnswersDecoration();
+    }, [props, options]);
 
     const SwitchIndex = () => {
         questionIndex < NUMBER_OF_QUESTIONS - 1 ?
@@ -25,32 +30,39 @@ const MultiChoiceQuestion: React.FC<MultiChoiceQuestionProps> = (props) => {
             setquestionIndex(0)
     }
 
-    const checkAnswer = (optionsList: HTMLUListElement) => {
+    const checkAnswer = (optionsList: HTMLUListElement, className: string = 'right-answer') => {
         const alternatives = Object.values(optionsList.children);
         alternatives.forEach(alternative => {
-            if (alternative.textContent === props.question.answer)
-                alternative.className = 'right-answer';
+            if (alternative.textContent === props.question.answer
+                || alternative.className === 'right-answer')
+                alternative.className = className;
         });
+    }
+
+    const clearAnswersDecoration = () => {
+        if (options.current) {
+            checkAnswer(options.current, '');
+        }
     }
 
     const handleSubmit = (e: React.FormEvent) => {
         e.preventDefault();
         if (options.current) {
-            checkAnswer(options.current)
+            checkAnswer(options.current);
         }
     }
 
     return (
         <div className="question-container">
             <span className="question-body">
-                {props.question.questions[questionIndex]}
+                {data.question.questions[questionIndex]}
                 <button onClick={SwitchIndex}>Rephrase</button>
             </span>
-            <form onSubmit={e => {handleSubmit(e)}}>
+            <form onSubmit={e => { handleSubmit(e) }}>
                 <ul ref={options}>
-                    {props.question.alternatives.map(item => (
+                    {data.question.alternatives.map(item => (
                         <li className="alternative">
-                            <input name={props.question.questions[0]} type="radio" value={item} />
+                            <input name={data.question.questions[0]} type="radio" value={item} />
                             <label htmlFor={item}>{item}</label>
                         </li>
                     ))}
